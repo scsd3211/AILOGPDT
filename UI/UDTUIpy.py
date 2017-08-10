485ec379da0a2a6853786356e176cca1e8ad1017
BU YU ER MO <binghunxuluo@qq.com>
2017-08-10 10:42:18 +0800

    just commit

--- UI/UDTUIpy.py
+++ UI/UDTUIpy.py
@@ -9,6 +9,13 @@ class Application(tk.Frame):
                           "NO way"]
         self.Labelm = [1, 2, 3, 4, 5, 6, 7];
         self.Encrym = [1, 2, 3, 4, 5, 6, 7];
+        self.strUDT = StringVar()
+        self.strPersonalorGroup = StringVar()
+        self.strSourceAddress = StringVar()
+        self.strTargetAddress = StringVar()
+        self.strEncryptornot = StringVar()
+        self.strOthers = StringVar()
+        self.strNOway= StringVar()
         self.root = master
         self.pack()
         #self.createWidgets()
@@ -30,6 +37,7 @@ class Application(tk.Frame):
         self.QUIT.pack(side="bottom")
     def packLabel(self):
         #self.Label[0] = tk.Label()
+        '''
         for tempi in range(7):
             #self.Labelm[tempi] = tk.Label(self.root, fg='red', bg='blue',text=self.Voicetext[tempi], width=40, height=2)
             self.Labelm[tempi] = tk.Label(self, fg='red', text=self.Voicetext[tempi], width=40, height=2,justify = LEFT)
@@ -37,6 +45,46 @@ class Application(tk.Frame):
             self.Labelm[tempi].grid(row=tempi, rowspan=1,columnspan=2)
             self.Encrym[tempi] =  tk.Entry(self,width=60)
             self.Encrym[tempi].grid(row=tempi,  column=3,rowspan=1, columnspan=2, )
+        '''
+
+
+        self.Labelm[0] = tk.Label(self, fg='red', text=self.Voicetext[0], width=40, height=2,justify = LEFT)
+        self.Labelm[0].grid(row=0, rowspan=1,columnspan=2)
+        self.Encrym[0] =  tk.Entry(self,width=60,textvariable=self.strUDT)
+        self.Encrym[0].grid(row=0,  column=3,rowspan=1, columnspan=2, )
+
+        self.Labelm[1] = tk.Label(self, fg='red', text=self.Voicetext[1], width=40, height=2,justify = LEFT)
+        self.Labelm[1].grid(row=1, rowspan=1,columnspan=2)
+        self.Encrym[1] =  tk.Entry(self,width=60,textvariable=self.strPersonalorGroup)
+        self.Encrym[1].grid(row=1,  column=3,rowspan=1, columnspan=2, )
+
+        self.Labelm[2] = tk.Label(self, fg='red', text=self.Voicetext[2], width=40, height=2,justify = LEFT)
+        self.Labelm[2].grid(row=2, rowspan=1,columnspan=2)
+        self.Encrym[2] =  tk.Entry(self,width=60,textvariable=self.strSourceAddress)
+        self.Encrym[2].grid(row=2,  column=3,rowspan=1, columnspan=2, )
+
+        self.Labelm[3] = tk.Label(self, fg='red', text=self.Voicetext[3], width=40, height=2,justify = LEFT)
+        self.Labelm[3].grid(row=3, rowspan=1,columnspan=2)
+        self.Encrym[3] =  tk.Entry(self,width=60,textvariable=self.strTargetAddress)
+        self.Encrym[3].grid(row=3,  column=3,rowspan=1, columnspan=2, )
+
+        self.Labelm[4] = tk.Label(self, fg='red', text=self.Voicetext[4], width=40, height=2,justify = LEFT)
+        self.Labelm[4].grid(row=4, rowspan=1,columnspan=2)
+        self.Encrym[4] =  tk.Entry(self,width=60,textvariable=self.strEncryptornot)
+        self.Encrym[4].grid(row=4,  column=3,rowspan=1, columnspan=2, )
+
+        self.Labelm[5] = tk.Label(self, fg='red', text=self.Voicetext[5], width=40, height=2,justify = LEFT)
+        self.Labelm[5].grid(row=5, rowspan=1,columnspan=2)
+        self.Encrym[5] =  tk.Entry(self,width=60,textvariable=self.strOthers)
+        self.Encrym[5].grid(row=5,  column=3,rowspan=1, columnspan=2, )
+
+        self.Labelm[6] = tk.Label(self, fg='red', text=self.Voicetext[6], width=40, height=2,justify = LEFT)
+        self.Labelm[6].grid(row=6, rowspan=1,columnspan=2)
+        self.Encrym[6] =  tk.Entry(self,width=60,textvariable=self.strNOway)
+        self.Encrym[6].grid(row=6,  column=3,rowspan=1, columnspan=2, )
+
+        #EncrymFile = tk.Entry(self,width=80,textvariable=self.Filee)
+        #EncrymFile.grid(row=4, column=2, rowspan=1, columnspan=8, )
 
     def say_hi(self):
         print("hi there, everyone!")
